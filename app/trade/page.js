cba03d0b71425093f20a6827e47f0b7ae6d2b512
osaosula <osaosula@yahoo.com>
2025-06-20 21:09:24 +0100

    Trade page updated

--- app/trade/page.js
+++ app/trade/page.js
@@ -1,3 +1,9 @@
+import Watchlist from "../_components/Watchlist";
+
 export default function Trade() {
-  return <h1>Trade</h1>;
+  return (
+    <div className="relative min-h-screen bg-[#2C2C2C]">
+      <Watchlist />
+    </div>
+  );
 }
